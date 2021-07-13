f43bea957f4332c5ce4469a7deb0ce3e626b2c14
jitendra19 <jiten.cs2010@gmail.com>
2021-07-13 21:58:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-util-funcs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-util-funcs)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// document.getElementById('inputText')
+
+
+
+
